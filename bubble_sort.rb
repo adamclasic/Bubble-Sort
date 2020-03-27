a9a756ccfcbd85279f335f3204ed8dcf77c0f174
adam allalou <aabou9@gmail.com>
2020-03-27 14:13:19 +0100

    update linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,9 @@ def bubble_sort(arr)
   j = 0
   while j < arr.length
     p = 0
-    while p < arr.length - 1 {arr[p], arr[p + 1] = arr[p + 1], arr[p] if arr[p] > arr[p + 1]}
+    while p < arr.length - 1
+      arr[p], arr[p + 1] = arr[p + 1], arr[p] if arr[p] > arr[p + 1]
+    end
     j += 1
   end
   arr
